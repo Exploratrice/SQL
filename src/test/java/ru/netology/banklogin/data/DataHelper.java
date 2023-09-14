e971d92c51e2006bdd9a0207611d331172d5c208
Daria <exploratrice2019@mail.ru>
2023-09-14 20:31:41 +0300

    initial commit

---
+++ src/test/java/ru/netology/banklogin/data/DataHelper.java
@@ -0,0 +1,70 @@
+package ru.netology.banklogin.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataHelper {
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfoWithTestData() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    private static String generateRandomLogin() {
+        return faker.name().username();
+    }
+
+    private static String generateRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static AuthInfo generateRandomUser() {
+        return new AuthInfo(generateRandomLogin(), generateRandomPassword());
+    }
+
+    public static VerificationCode generateRandomVerificationCode() {
+        return new VerificationCode(faker.numerify("######"));
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+    }
+}
+
+
